957754888e62bdd48998d4bb03f7f3478cf8a8b3
Thanoscar1 <jdonell62@gmail.com>
2020-02-05 23:23:31 +0000

    yah boi

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var song
+var artist
+var length
+var image
+var link
+var add
+let playlist = [song, artist,length,image,link,add]
+let song = ['swept up','kings bread','sunflower','Over now','last suprise'];
+song.forEach(function(data) {
+    $(#songs).append("<li>"+data+"</li>")
+let artist = ['young cash resgister aka lilbroomstick','young cash register aka lil broomstick baku bank bill','Post malone Swae Lee ','Post malone','Lyn Inaizumi'];
+artist.forEach(function(data) {
+    $(#artist).append("<li>"+data+"</li>")
+let length = ['2:05','2:00','sunflower','Over now','last suprise'];
+song.forEach(function(data) {
+    $(#songs).append("<li>"+data+"</li>")       
+});
